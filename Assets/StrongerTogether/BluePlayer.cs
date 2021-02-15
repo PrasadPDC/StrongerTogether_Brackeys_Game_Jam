234868fe75ec9f3363d4152d51901865ee28c670
Prasad_Chandole <73127970+PrasadPDC@users.noreply.github.com>
2021-02-15 21:05:01 +0530

    created new repository
    new repository

---
+++ Assets/StrongerTogether/BluePlayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BluePlayer : MonoBehaviour
+{
+    [SerializeField]private float moveSpeed = 100f;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rb.MovePosition(transform.position + transform.forward * moveSpeed * Time.fixedDeltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            rb.MovePosition(transform.position + transform.right * moveSpeed * Time.fixedDeltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            rb.MovePosition(transform.position + -transform.right * moveSpeed * Time.fixedDeltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            rb.MovePosition(transform.position + -transform.forward * moveSpeed * Time.fixedDeltaTime);
+        }
+    }
+}
---
+++ Assets/StrongerTogether/RedPlayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedPlayer : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rb.MovePosition(transform.position + transform.forward * moveSpeed * Time.fixedDeltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            rb.MovePosition(transform.position + transform.right * moveSpeed * Time.fixedDeltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            rb.MovePosition(transform.position + -transform.right * moveSpeed * Time.fixedDeltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            rb.MovePosition(transform.position + -transform.forward * moveSpeed * Time.fixedDeltaTime);
+        }
+    }
+}
