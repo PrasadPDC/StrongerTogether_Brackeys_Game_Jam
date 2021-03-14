5a70b32fae15a7d2719ee5f03e2028b1d61eb8b5
Prasad_Chandole <73127970+PrasadPDC@users.noreply.github.com>
2021-03-14 22:57:19 +0530

    final game
    final game

---
+++ Assets/StrongerTogether/Scripts/BluePlayer.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BluePlayer : MonoBehaviour
+{
+    [SerializeField]private float moveSpeed = 100f;
+    [SerializeField] private float turnSmoothtime = 0.1f;
+
+    public CharacterController controller;
+    private UI_System UI;
+    float turnVelocity;
+    public Transform Camera;
+    public float TimeForNextLevel;
+    
+    void Start()
+    {
+        UI = FindObjectOfType<UI_System>();        
+    }
+
+    void Update()
+    {
+
+
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(x, 0, y).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float rotation = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + Camera.eulerAngles.y;
+            float rot = Mathf.SmoothDampAngle(transform.eulerAngles.y, rotation, ref turnVelocity, turnSmoothtime);
+            transform.rotation = (Quaternion.Euler(new Vector3(0, rot, 0)));
+            
+            Vector3 MoveDir = Quaternion.Euler(0, rotation, 0) * Vector3.forward;
+            controller.Move(MoveDir.normalized * moveSpeed * Time.deltaTime);
+        }
+      
+    }
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+       
+        if (hit.gameObject.CompareTag("Portal"))
+        {
+            StartCoroutine(nextLevel());
+
+        }
+    }
+
+    IEnumerator nextLevel()
+    {
+        yield return new WaitForSeconds(TimeForNextLevel);
+        UI.LevelComplete = true;
+    }
+}
---
+++ Assets/StrongerTogether/Scripts/UI_System.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class UI_System : MonoBehaviour
+{
+    public TextMeshProUGUI Keys;
+    [HideInInspector]public bool LevelComplete = false;
+    public GameObject GATE;
+    public GameObject Portal;
+    private int key = 3;
+    public GameObject[] ToHide = new GameObject[5];
+
+    public int KeyCount
+    {
+
+        get { return key; }
+        set { key = value; }
+    }
+    void Start()
+    {
+        LevelComplete = false;
+        Portal.SetActive(false);
+        ToHide[0].SetActive(true);
+        ToHide[1].SetActive(true);
+        ToHide[2].SetActive(true);
+        ToHide[3].SetActive(true);
+        ToHide[4].SetActive(true);
+    }
+
+    void Update()
+    {
+      
+
+        if (LevelComplete)
+        {
+            SceneManager.LoadScene( SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        if(key == 0)
+        {
+            GATE.GetComponent<BoxCollider>().enabled = false;
+            Portal.SetActive(true);
+        }
+
+        Keys.text = "TotalGems:" + KeyCount;
+    }
+    private void LateUpdate()
+    {
+        KeyCount = GameObject.FindGameObjectsWithTag("Key").Length;
+    }
+
+    public void next()
+    {
+        ToHide[0].SetActive(true);
+        ToHide[1].SetActive(false);
+    }
+    public void pre()
+    {
+        ToHide[0].SetActive(false);
+        ToHide[1].SetActive(true);
+    }
+    public void cut()
+    {
+        ToHide[0].SetActive(false);
+        ToHide[1].SetActive(false);
+        ToHide[2].SetActive(false);
+        ToHide[3].SetActive(false);
+        ToHide[4].SetActive(false);
+    }
+}
